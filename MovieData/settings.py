0889b21a5867cbc26de384b9b9237975fc7918fe
Han <BruceHenry@users.noreply.github.com>
2017-06-17 00:10:35 -0400

    static_root

--- MovieData/settings.py
+++ MovieData/settings.py
@@ -119,3 +119,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
